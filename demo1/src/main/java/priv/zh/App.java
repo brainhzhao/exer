ed6b1e28e49f2fafb333cfce268ae88cee5f5a08
zhaoheng <zhaohengb@dcits.com>
2018-12-30 20:02:29 +0800

    删除不需要托管的库

--- demo1/src/main/java/priv/zh/App.java
+++ demo1/src/main/java/priv/zh/App.java
@@ -4,10 +4,10 @@ package priv.zh;
  * Hello world!
  *
  */
-public class App 
+public class App  
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" ); 
+        System.out.println( "Hello World123!" ); 
     }
 }
