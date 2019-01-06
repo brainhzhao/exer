9d40e0deed789db08e17b6fd8bbf44bc0360e57c
zhaoehengb <zhaohengb@dcits.com>
2019-01-06 14:47:02 +0800

    ThreadLocal类的一次练习

--- demo1/src/main/java/priv/zh/App.java
+++ demo1/src/main/java/priv/zh/App.java
@@ -1,7 +1,9 @@
 package priv.zh;
 
+
 import priv.zh.model.HelloWorld;
 import priv.zh.model.HelloWorldService;
+import priv.zh.thread.ThreadLocalDemo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,11 +14,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App  
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) 
     {
-        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        HelloWorldService service=(HelloWorldService)context.getBean("helloWorldService");
-        HelloWorld helloWorld=service.getHelloWorld();
-        helloWorld.sayHello();
+        try {
+         /*   ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+            HelloWorldService service=(HelloWorldService)context.getBean("helloWorldService");
+            HelloWorld helloWorld=service.getHelloWorld();
+            helloWorld.sayHello();*/
+            ThreadLocalDemo localDemo=new ThreadLocalDemo();
+            localDemo.start();
+        } catch (Exception e) {
+
+        }      
     }
 }
---
+++ demo1/src/main/java/priv/zh/thread/ThreadLocalDemo.java
@@ -0,0 +1,51 @@
+package priv.zh.thread;
+
+
+import java.lang.ThreadLocal;
+import java.lang.Runnable;
+
+/*
+  @author zhaoheng
+  @propose exer 
+*/
+
+//ThreadLocal 可以实现线程的私有变量
+
+public class ThreadLocalDemo{
+
+    //实例化一个线程私有变量的对象生成器
+    private ThreadLocal<Integer> seqNum=new ThreadLocal<Integer>(){
+        @Override
+        public Integer initialValue(){
+            return 0;
+        }
+    };
+    
+    public void start(){
+        Thread thread1=new Thread(new ThreadDemo(seqNum));
+        Thread thread2=new Thread(new ThreadDemo(seqNum));
+        Thread thread3=new Thread(new ThreadDemo(seqNum));
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+    }
+
+
+    private static class ThreadDemo implements Runnable{
+        private ThreadLocal<Integer> seq;
+
+        public ThreadDemo(ThreadLocal<Integer> seq){
+            this.seq=seq;
+        }
+        @Override 
+        public void run(){
+            for(int i=0;i<=5;i++){
+                System.out.printf("Current Thread is %s,seq is [%d]\n",Thread.currentThread().getName(),seq.get());
+                seq.set(seq.get()+1);
+            }
+        }
+    }
+
+}
