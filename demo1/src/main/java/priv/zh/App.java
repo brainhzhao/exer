591fcb90d7f22d5e9a5489c8d18e45dc7190106a
zhaoheng <zhaohengb@dcits.com>
2019-01-12 14:21:57 +0800

    提交多线程练习代码
    练习阴式锁和显式锁

--- demo1/src/main/java/priv/zh/App.java
+++ demo1/src/main/java/priv/zh/App.java
@@ -1,11 +1,8 @@
 package priv.zh;
 
 
-import priv.zh.model.HelloWorld;
-import priv.zh.model.HelloWorldService;
 import priv.zh.thread.ThreadLocalDemo;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 /*
 @author SilenceZhao
@@ -17,10 +14,10 @@ public class App
     public static void main( String[] args ) 
     {
         try {
-         /*   ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+            ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
             HelloWorldService service=(HelloWorldService)context.getBean("helloWorldService");
             HelloWorld helloWorld=service.getHelloWorld();
-            helloWorld.sayHello();*/
+            helloWorld.sayHello();
             ThreadLocalDemo localDemo=new ThreadLocalDemo();
             localDemo.start();
         } catch (Exception e) {
---
+++ demo1/src/main/java/priv/zh/thread/lockdemo/ReentrantLockDemo.java
@@ -0,0 +1,16 @@
+package priv.zh.thread.lockdemo;
+
+
+/*
+
+@author zhaoheng
+目的：
+学习一下显式锁
+ Lock 接口提供了无条件的、可轮询的、定时的、可中断的锁获取操作，所有加锁、解锁方式都是显式的
+ Lock包位于 java.lang.concurrent.locks.Lock中
+ 其核心方法是 lock()、unlock()、tryLock()
+     实现类有ReentrantLock、ReentrantReadWriteLock.ReadLock、ReentrantReadWrite.Writelock
+*/
+public class ReentrantLockDemo {
+
+}
