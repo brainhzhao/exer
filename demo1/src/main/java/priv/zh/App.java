3f84ca2abe2fca170dd5b17f43189b2be74a3f3b
zhaoheng <silencehengzhao@gmail.com>
2019-01-16 11:17:33 +0800

    commit by zhaoheng

--- demo1/src/main/java/priv/zh/App.java
+++ demo1/src/main/java/priv/zh/App.java
@@ -4,7 +4,7 @@ import priv.zh.model.HelloWorld;
 import priv.zh.model.HelloWorldService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import net.sf.jasperreports.engine.JasperCompileManager;
 /*
 @author SilenceZhao
 @propose exer Spring
@@ -12,11 +12,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App  
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
+        /*
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
         HelloWorldService service=(HelloWorldService)context.getBean("helloWorldService");
         HelloWorld helloWorld=service.getHelloWorld();
         helloWorld.sayHello();
+        */
+        JasperCompileManager.compileReportToFile("/media/heng/SilenceZhao/code/java/zhdemo/demo1/target/classes/PISA_BASELIST.jrxml", "/media/heng/SilenceZhao/pingan_app/SmartThemis/SmartThemis_src/webapp/WEB-INF/jasper/PISA_BASELIST.jasper");
+        System.out.println("ok");
     }
 }
