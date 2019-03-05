1814ae054d4efdb8384dcfb428d97488407668ee
zhaoheng <zhaohengb@dcits.com>
2019-03-05 21:57:36 +0800

    commit by zhaoheng

--- ui/js/demo/demo1.js
+++ ui/js/demo/demo1.js
@@ -1,1 +1,10 @@
-alert(2113);
+
+window.onload=function(){
+    var check=document.getElementById("alertItemChecked");
+
+    check.addEventListener("click",function(e){
+        var items=document.getElementsByName("item");
+        console.log(items.entries())
+        
+    }) 
+}
